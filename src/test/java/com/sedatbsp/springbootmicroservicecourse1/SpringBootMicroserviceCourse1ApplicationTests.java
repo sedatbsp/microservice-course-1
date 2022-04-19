4a6ba19ccf257b7260898764633594bed5064f54
Sedat Başpınar <sdtbsp10@gmail.com>
2022-04-19 05:33:59 +0300

    init

---
+++ src/test/java/com/sedatbsp/springbootmicroservicecourse1/SpringBootMicroserviceCourse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedatbsp.springbootmicroservicecourse1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceCourse1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
